03961e0b79b8944c7b8053717a1594d782b9818a
karthick_raja <233344860+kbangaru-cyber@users.noreply.github.com>
2026-02-01 10:45:52 -0500

    Add FlowMatching to __all__ in methods module
    Added FlowMatching to the methods module exports.

--- src/methods/__init__.py
+++ src/methods/__init__.py
@@ -3,12 +3,16 @@ Methods module for cmu-10799-diffusion.
 
 This module contains implementations of generative modeling methods:
 - DDPM (Denoising Diffusion Probabilistic Models)
+- Flow Matching
 """
 
 from .base import BaseMethod
 from .ddpm import DDPM
+from .flow_matching import FlowMatching
+
 
 __all__ = [
-    'BaseMethod',
-    'DDPM',
+    "BaseMethod",
+    "DDPM",
+    "FlowMatching",
 ]
